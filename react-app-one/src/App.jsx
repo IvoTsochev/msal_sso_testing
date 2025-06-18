9d66e9b139977b62602b43c4a7880830f8a56f19
Ivaylo Tsochev <36713516+IvoTsochev@users.noreply.github.com>
2025-06-18 16:17:39 +0300

    Update App.jsx

--- react-app-one/src/App.jsx
+++ react-app-one/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
       {accounts.length > 0 ? (
         <p>Logged in as: {accounts[0].username}</p>
       ) : (
-        <p>Authenticating...</p> // You could also show a loader here
+        <p>Authenticating...</p>
       )}
     </div>
   );
@@ -41,54 +41,3 @@ function App() {
 export default App;
 
 
-
-
-
-
-
-
-
-
-
-
-// import { useEffect } from "react";
-// import { useMsal } from "@azure/msal-react";
-// import "./App.css";
-
-// function App() {
-//   const { instance, accounts, inProgress } = useMsal();
-
-//   useEffect(() => {
-//     if (inProgress === "none" && !accounts.length) {
-//       instance
-//         .ssoSilent({
-//           scopes: ["user.read"],
-//           loginHint: "your-username@headlessteam.onmicrosoft.com",
-//           redirectUri: "http://localhost:3000/blank.html",
-//         })
-//         .then((response) => {
-//           instance.setActiveAccount(response.account);
-//         })
-//         .catch((error) => {
-//           console.warn("SSO silent failed", error);
-//         });
-//     }
-//   }, [inProgress, accounts.length, instance]);
-
-//   const handleLogin = () => {
-//     instance.loginRedirect({ scopes: ["openid", "profile", "user.read"] });
-//   };
-
-//   return (
-//     <div>
-//       <h1>Welcome to My React App ONE</h1>
-//       {accounts.length > 0 ? (
-//         <p>Logged in as: {accounts[0].username}</p>
-//       ) : (
-//         <button onClick={handleLogin}>Login</button>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
